6f7e8006fbf7e56b58eb77327050145780bb6685
Dekta <gilvamgsj@gmail.com>
2022-02-22 11:22:48 -0300

    alteração de pasta

---
+++ Atv3/teste.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=0, b=0;
+	int resultado=0;
+
+	cin >> a >> b;
+
+	if(a==b)
+	{
+		resultado = a+b;
+	}
+	else
+	{
+		resultado = a*b;
+	}
+
+	cout << resultado;
+
+	return 0;
+}
